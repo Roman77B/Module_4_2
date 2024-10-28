932ed9fa69088b82570527c81a7ceeea6f4d8ced
Roman77B <rbodrov@gmail.com>
2024-10-28 17:40:57 +0300

    DZ https://urban-university.pro/student/lesson/421

--- module_4_2.py
+++ module_4_2.py
@@ -1,16 +1,22 @@
 # Домашнее задание по уроку "Пространство имен"
 # 1. Создайте новую функцию test_function
+
 def test_function():
     # 2. Создайте внутри test_function другую функцию - inner_function,
     def inner_function():
         # Эта функция должна печатать значение "Я в области видимости функции test_function"
         print("Я в области видимости функции test_function")
+        print(test_function.__name__)
 
     # 3. Вызовите функцию inner_function внутри функции test_function
     inner_function()
+
 
 test_function()
 
 # 4. Попробуйте вызывать inner_function вне функции test_function
 # и посмотрите на результат выполнения программы
-inner_function()
+try:
+    inner_function()
+except NameError as e:
+    print(e)
